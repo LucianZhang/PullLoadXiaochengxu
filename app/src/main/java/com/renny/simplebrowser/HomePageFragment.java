3e4817e74296337a7890ada43bb17dd0483d9c77
renjn <renjianan@aixuedai.com>
2017-12-29 15:17:20 +0800

    扫一扫

--- app/src/main/java/com/renny/simplebrowser/HomePageFragment.java
+++ app/src/main/java/com/renny/simplebrowser/HomePageFragment.java
@@ -1,8 +1,14 @@
 package com.renny.simplebrowser;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -13,7 +19,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.renny.zxing.Activity.CaptureActivity;
+
 import java.util.regex.Pattern;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -25,7 +35,7 @@ public class HomePageFragment extends Fragment implements View.OnClickListener {
     private String homePage = "https://juejin.im/user/5795bb80d342d30059f14b1c";
     private String baidu = "https://www.baidu.com/";
     private String github = "https://github.com/renjianan/SimpleBrowser";
-
+    private static final int REQUEST_SCAN = 0;
     EditText mEditText;
 
     @Nullable
@@ -44,24 +54,61 @@ public class HomePageFragment extends Fragment implements View.OnClickListener {
         rootView.findViewById(R.id.profile).setOnClickListener(this);
         rootView.findViewById(R.id.baidu).setOnClickListener(this);
         rootView.findViewById(R.id.github).setOnClickListener(this);
+        rootView.findViewById(R.id.scan).setOnClickListener(this);
         mEditText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String text = mEditText.getText().toString();
                 if (actionId == EditorInfo.IME_ACTION_GO && mGoPageListener != null) {
-                    String text = mEditText.getText().toString();
-                    if (TextUtils.isEmpty(text)) {
-                        Toast.makeText(getActivity(), "请输入网址", Toast.LENGTH_SHORT).show();
-                    } else if (!isUrl(text)) {
-                        mGoPageListener.onGopage("http://www.baidu.com/s?wd=" + text);
-                    } else {
-                        mGoPageListener.onGopage(text);
-                    }
+                    startBrowser(text);
                     return true;
                 }
                 return false;
             }
         });
 
+    }
+
+    private void startBrowser(String text) {
+        if (mGoPageListener != null) {
+            if (TextUtils.isEmpty(text)) {
+                Toast.makeText(getActivity(), "请输入网址", Toast.LENGTH_SHORT).show();
+            } else if (!isUrl(text)) {
+                mGoPageListener.onGopage("http://www.baidu.com/s?wd=" + text);
+            } else {
+                mGoPageListener.onGopage(text);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    jumpScanPage();
+                } else {
+                    Toast.makeText(getActivity(), "拒绝", Toast.LENGTH_LONG).show();
+                }
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SCAN && resultCode == RESULT_OK) {
+            startBrowser(data.getStringExtra("barCode"));
+        }
+    }
+
+    /**
+     * 跳转到扫码页
+     */
+    private void jumpScanPage() {
+        startActivityForResult(new Intent(getActivity(), CaptureActivity.class), REQUEST_SCAN);
     }
 
     public void setGoPageListener(goPageListener goPageListener) {
@@ -82,7 +129,21 @@ public class HomePageFragment extends Fragment implements View.OnClickListener {
                 case R.id.github:
                     mGoPageListener.onGopage(github);
                     break;
+                case R.id.scan:
+                    getRuntimeRight();
+                    break;
             }
+        }
+    }
+
+    /**
+     * 获得运行时权限
+     */
+    private void getRuntimeRight() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, 1);
+        } else {
+            jumpScanPage();
         }
     }
 
